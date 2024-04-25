506571c61e609ba4654da90b1ead963914d26caa
Christian Manzaraz <magnusmanz@gmail.com>
2024-04-25 12:31:36 -0300

    Initial Commit

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Christian Manzaraz on 25/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
