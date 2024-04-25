53b8b8662e8eddb51c1d8f01cc4149523b092465
Christian Manzaraz <magnusmanz@gmail.com>
2024-04-25 19:38:00 -0300

    Section 4 Use a ZStack to embellish your graphic Add an SF Symbol on top of your shape using a ZStack. A ZStack arranges views on top of one another.

--- WeatherForecast/ContentView.swift
+++ WeatherForecast/ContentView.swift
@@ -12,23 +12,25 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            RoundedRectangle(cornerRadius: 30)
-                .frame(width: 150, height: 150)
-                .foregroundStyle(.tint)
+            ZStack {
+                RoundedRectangle(cornerRadius: 30)
+                    .frame(width: 150, height: 150)
+                    .foregroundStyle(.tint)
+                
+                Image(systemName: "figure.2.and.child.holdinghands")
+                    .font(.system(size: 70))
+                    .foregroundStyle(.white)
+            }
             
             Text("Welcome to MyApp")
                 .font(.title)
                 .fontWeight(.semibold)
                 .padding(.top)
-                .border(.black, width: 1.5)
             
             Text("Description Text")
                 .font(.title2)
-                .border(.black, width: 1.5)
         }
-        .border(.orange, width: 1.5)
         .padding()
-        .border(.purple, width: 1.5)
     }
 }
 
